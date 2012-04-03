658e516f30efba09e1a67435c38e809285b0a21b
Nathan Buskirk <nbuskirk@gmail.com>
2012-04-02 23:18:53 -0400

    added flicker animation effect to combat

--- resources/js/monsters.js
+++ resources/js/monsters.js
@@ -4,16 +4,20 @@ Crafty.c("monsterai",{
 		current:60,
 		max:60
 	},
+	combat:false,
 	
 	init:function(){
-		this.requires("SpriteAnimation, Collision, 2D, Canvas")	
+
+		this.requires("SpriteAnimation, Collision, 2D, Canvas, Flicker")	
 		.onHit("hero",function(ent){
             var player = ent[0].obj;
             Crafty(player[0]).trigger("Hurt",this.hp);
             this.trigger("Hurt",this.hp);
         })
         .bind("Hurt",function(dmg){
-            this.hp.current--;
+	    this.combat=true;
+	    this.flicker = true;
+	    this.hp.current--;
             if(this.hp.current <= 0) this.trigger("Die");
         })
         .bind("Die",function(){
@@ -23,7 +27,14 @@ Crafty.c("monsterai",{
         .bind("Reset",function(){
         	Crafty.e("2D,Canvas,spr_floor1").attr({x:this.x,y:this.y}); 
             this.hp.current = this.hp.max;      	
-        });
+        })
+	.bind("EnterFrame",function(){ 
+		if(this.combat){
+		this.flicker = true;
+		}else {
+		this.flicker = false;
+		}
+	});
 	},
 
 })
